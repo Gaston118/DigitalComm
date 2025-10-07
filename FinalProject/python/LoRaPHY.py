30a2f9c4805b17d77b8ccdcc026aff0457c75674
Gaston118 <gaston.capdevila@mi.unc.edu.ar>
2025-10-06 23:24:35 -0300

    Formado del header

--- FinalProject/python/LoRaPHY.py
+++ FinalProject/python/LoRaPHY.py
@@ -11,3 +11,9 @@ CRC = 0
 
 symbols = lora_encode(payload, SF=SF, CR=CR, LDRO=DE, IH=IH, CRC=CRC, verbose=True)
 print("Cantidad de símbolos generados:", len(symbols))
+
+# Header generado (nibbles): [0 5 8 0 9] → bits: [0000 0101 1000 0000 1001]
+# Primeros 2 nibbles: payload length = 5 → [0 5]
+# Nibble 3: CR=4/8 (100) y Payload CRC enable = 0 → [8] (1000)
+# Nibble 4: 3 bits reservados + 1er bit del checksum → [0]
+# Nibble 5: bits 1-4 del CRC header → [9]
